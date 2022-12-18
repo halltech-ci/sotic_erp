864b99d65ae0ffee879837cced55f8de85cd8cdd
ATCHE Maurice <maurice.atce@halltech-africa.com>
2022-12-18 02:21:02 +0000

    Updating custom_sale_uom

--- Sale_management/hta_sale_approval/__manifest__.py
+++ Sale_management/hta_sale_approval/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale'],
+    'depends': ['sale_management'],
 
     # always loaded
     'data': [
