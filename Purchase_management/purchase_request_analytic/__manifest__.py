ac71f3f7174d73b9974e0712c799e5dbab98c3e0
ATCHE MAurice <maurice.atche@halltech-africa.com>
2023-01-03 10:46:55 +0000

    Updating purchase_request_analytic

--- Purchase_management/purchase_request_analytic/__manifest__.py
+++ Purchase_management/purchase_request_analytic/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['purchase_request', 'purchase_request_sale],
+    'depends': ['purchase_request', 'purchase_request_sale',],
 
     # always loaded
     'data': [
