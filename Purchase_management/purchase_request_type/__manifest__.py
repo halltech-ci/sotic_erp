438337888a7aa8ba91de9f057569d903230e524a
ATCHE Maurice <maurice.atche@halltech-africa.com>
2023-01-08 06:36:10 +0000

    Adding purchase_request in purchase_request_type

--- Purchase_management/purchase_request_type/__manifest__.py
+++ Purchase_management/purchase_request_type/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['purchase_request'],
 
     # always loaded
     'data': [
