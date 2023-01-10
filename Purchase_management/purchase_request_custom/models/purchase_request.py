63b5ee60728b5c7c8ea5bfe7387972b90459f5b2
ATCHE Maurice <maurice.atche@halltech-africa.com>
2023-01-10 01:26:12 +0000

    Adding view in purchase_request_custom

--- Purchase_management/purchase_request_custom/models/purchase_request.py
+++ Purchase_management/purchase_request_custom/models/purchase_request.py
@@ -22,3 +22,32 @@ class PurchaseRequestLine(models.Model):
     product_tmpl_id = fields.Many2one("product.template", related="product_id.product_tmpl_id", store=True)
     attribute_line_ids = fields.One2many("product.template.attribute.line", related="product_tmpl_id.attribute_line_ids")
     specifications = fields.Text(default="")
+    
+    @api.onchange("product_id")
+    def onchange_product_id(self):
+        for rec in self:
+            if rec.product_id:
+                name = rec.product_id.name
+                if rec.product_id.code:
+                    name = "{} ".format(rec.product_id.product_tmpl_id.name)
+                    for no_variant_attribute_value in rec.product_id.product_template_attribute_value_ids:
+                        name += "{}".format(no_variant_attribute_value.name + ', ')
+                if rec.product_id.description_purchase:
+                    name += "\n" + rec.product_id.description_purchase
+                rec.product_uom_id = rec.product_id.uom_id.id
+                rec.product_qty = 1
+                rec.name = name
+    
+    @api.constrains('product_id', 'product_uom_id')
+    def _compare_product_uom(self):
+        #for line
+        if self.product_id:
+            if self.product_id.uom_id.category_id != self.product_uom_id.category_id:
+                raise ValidationError("Les unite de mesure de %s ne sont pas dans la meme categorie" % (self.product_id.name))
+    
+    
+    @api.onchange('product_uom_id')
+    def _onchange_product_uom(self):
+        if self.product_id:
+            if self.product_id.uom_id.category_id != self.product_uom_id.category_id:
+                raise ValidationError("Les unite de mesure de %s ne sont pas dans la meme categorie" % (self.product_id.name))
